dc4852db6bb6da4879bbd6fb5affe9a015048803
Navonil Mukherjee <navonil_mukherjee@yahoo.co.in>
2018-03-09 17:52:33 +0530

    DDA line drawing algorithm

--- dda.cpp
+++ dda.cpp
@@ -3,8 +3,6 @@
 #include "windows.h"
 #include "graphics.h"
 
-// #define X(n) (getmaxx()/2 + n)
-// #define Y(n) (getmaxy()/2 - n)
 
 typedef struct point
 {
@@ -24,8 +22,6 @@ int main()
     printf("Please enter the coordinates for the end point of the line.\n");
 	scanf("%d%d", &end.x, &end.y);
 	initwindow(600, 600, "DDA");
-	// line(getmaxx() / 2, 0, getmaxx() / 2, getmaxy());
-	// line(0, getmaxy()/2, getmaxx(), getmaxy()/2);
 	setcolor(3);
 	circle(start.x, start.y, 3);
 	circle(end.x, end.y, 3);
@@ -38,40 +34,49 @@ int main()
     
 void dda(Point start, Point end)
 {
+	double slope = 0.0, x = 0.0, y = 0.0;
 	int dy = end.y - start.y, dx = end.x - start.x, sgn = 0;
 	std::cout << dy << " " << dx << std::endl;
-	double slope = 0.0;
-	if (start.x > end.x)
-		swap(start, end);
+
 	if (dx == 0)
-		for (; start.y != end.y; ++start.y)
-			putpixel(start.x, start.y, 12);
+		for (y = start.y; y <= end.y; ++y)
+			putpixel((int)x, (int)y, 12);
 	else if (dy == 0)
-		for (; start.x != end.x; ++start.x)
-			putpixel(start.x, start.y, 12);
+		for (x = start.x; x <= end.x; ++x)
+			putpixel((int)x, (int)y, 12);
 	else
 	{
 		if (abs(dy) <= abs(dx)) // gentle slope
 		{
+			if (start.x > end.x) // swaps the starting point if needed
+				swap(start, end);
+
+			putpixel(start.x, start.y, 12); // draws the initial point
+
 			slope = dy /(double) dx;
 			sgn = sign(slope);
-			std::cout << "CASE 1 Slope : " << slope << " Sign : " << sgn << std::endl;
-			for (; start.x != end.x; start.x += sgn)
+			
+			for (x = start.x, y = start.y; x <= end.x; x += sgn)
 			{
-				start.y = static_cast<int>(start.y + slope);
-				putpixel(start.x, start.y, 12);
+				y = y + slope;
+				putpixel((int)x, (int)y, 12);
 			}
 		}
 
 		else // steep slope
 		{
-			slope = dx /(double) dy;
+			if (start.y > end.y) // swaps the starting point if needed
+				swap(start, end);
+
+			putpixel(end.x, end.y, 12); // draws the initial point
+
+			slope = dx /(double) dy;  //	1 / m
 			sgn = sign(slope);
-			std::cout << "CASE 2 Slope : " << slope << " Sign : " << sgn << std::endl;
-			for (; start.y != end.y; start.y += sgn)
+			
+			for (x = start.x, y = start.y; y <= end.y; y += sgn)
 			{
-				start.x = static_cast<int>(start.x + slope);
-				putpixel(start.x, start.y, 12);
+				x = x + slope;
+				putpixel((int)x, (int)y, 12);
 			}
 		}
 
