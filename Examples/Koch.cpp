6a656a1ec3311aa367937aa9adeaaa40be95bcc2
Navonil Mukherjee <navonil_mukherjee@yahoo.co.in>
2022-07-09 04:45:09 +0530

    Koch Snowflake - WIP

---
+++ Examples/Koch.cpp
@@ -0,0 +1,114 @@
+/* The following program renders the Koch Snowflake .*/
+
+#include <iostream>
+#include <windows.h>
+#include <cmath>
+#include <vector>
+
+#include "graphics.h"
+#include "colors.h"
+#include "primitives.h"
+
+// trisect a line segment and return the points
+std::vector<Point> trisect(const Line& edge)
+{
+	Point p1;
+	double x = ((double)edge.dst.x + 2 * (double)edge.src.x) * 0.3333; // (mx2 + nx1) / (m + n)
+	double y = ((double)edge.dst.y + 2 * (double)edge.src.y) * 0.3333; // (my2 + ny1) / (m + n)
+
+	p1.x = static_cast<int>(x);
+	p1.y = static_cast<int>(y);
+
+	Point p2 = getMidpoint(p1, edge.dst);
+	std::vector<Point> outList = {edge.src, p1, p2, edge.dst};
+	return outList;
+}
+
+/*
+	Given any two points p1 and p2 in an equilateral triangle,
+	get the third point.
+*/
+Point getApex(const Point& p1, const Point& p2)
+{	
+	Vec2 dir = points2Vec(p1, p2);	// get direction vector from p1 to p2
+
+	Point mid = getMidpoint(p1, p2);
+	Vec2 o(mid.x, mid.y);
+	double a = dir.getMagnitude(); // get the length of each side of the equilateral triangle
+	double t = 0.86605 * a; // height (h) = (sqrt(3) / 2) * a
+
+	Vec2 normal = dir.getNormal();
+	Vec2 p = o + normal * t; // walk h units from the mid point in the direction of the apex
+	return vec2Point(p);
+}
+
+// return a Fractal side for a given edge
+std::vector<Line> getFractalSide(const Line& edge)
+{
+	std::vector<Point> splitPoints = trisect(edge);
+	Point apex = getApex(splitPoints[1], splitPoints[2]);
+
+	std::vector<Line> out;
+	out.push_back(Line(splitPoints[0], splitPoints[1]));
+	out.push_back(Line(splitPoints[1], apex));
+	out.push_back(Line(apex, splitPoints[2]));
+	out.push_back(Line(splitPoints[2], splitPoints[3]));
+
+	return out;
+}
+
+// draws each Koch curve of the snowflake
+void drawFractalSide(const std::vector<Line>& edgeList)
+{
+	setcolor(BLACK);
+	line(edgeList[0].dst.x, edgeList[0].dst.y, edgeList[3].src.x, edgeList[3].src.y); // remove split edge from triangle
+	setcolor(WHITE);
+
+	for (const auto& edge : edgeList)
+		edge.draw();
+}
+
+void drawKochSnowflake(const std::vector<Line>& edgeList, int depth, int maxDepth)
+{
+	if (depth < maxDepth)
+	{
+		// for (const auto& edge : edgeList)
+			// edge.draw();
+
+		std::vector<Line> fractalEdges;
+
+		for (const auto& edge : edgeList)
+		{
+			std::vector<Line> sideList = getFractalSide(edge);
+			drawFractalSide(sideList);
+
+			for (const auto& side : sideList)
+				fractalEdges.push_back(side);
+		}
+
+		drawKochSnowflake(fractalEdges, depth + 1, maxDepth);
+	}
+}
+
+int main()
+{
+	initwindow(640, 480, "Koch Snowflake");
+	int ch = 0;
+	uint16_t nItr = 0;
+
+	std::cout << "This program renders the Koch Snowflake with animation." << std::endl;
+	std::cout << "Please input the desired number of interations." << std::endl;
+	std::cin >> nItr;
+
+	std::vector<Line> edges;
+	edges.push_back({{ 220, 120 }, { 420, 120 }});
+	edges.push_back({{ 420, 120 }, { 320, 294 }});
+	edges.push_back({{ 320, 294 }, { 220, 120 }});
+
+	drawKochSnowflake(edges, 0, nItr);			
+		
+	std::cout << "Thank you." << std::endl;
+	system("pause"); // windows only feature
+	closegraph();
+	return 0;
+}
--- primitives.h
+++ primitives.h
@@ -2,17 +2,38 @@
 #ifndef PRIMITIVES__H_
 #define PRIMITIVES__H_
 
-typedef struct 
+#include <cmath>
+
+struct Point
 {
 	int x;
 	int y;
-} Point;
 
-typedef struct 
+	Point() : x(0), y(0) {}
+	Point(const int &x_, const int &y_) : x(x_), y(y_) {}
+};
+
+Point getMidpoint(const Point& p1, const Point& p2)
+{
+	Point out = {};
+	out.x = static_cast<int>(round(0.5 * ((double)p1.x + (double)p2.x)));
+	out.y = static_cast<int>(round(0.5 * ((double)p1.y + (double)p2.y)));
+	return out;
+}
+
+struct Line
 {
 	Point src;
 	Point dst;
-} Line;
+
+	Line(const Point& src_, const Point& dst_) : src(src_), dst(dst_) {}
+	Line(int x1, int y1, int x2, int y2) : src(Point(x1, y1)), dst(Point(x2, y2)) {}
+
+	void draw() const
+	{
+		line(src.x, src.y, dst.x, dst.y);
+	}
+};
 
 typedef struct
 {
@@ -26,5 +47,84 @@ typedef struct
 	Point b;
 	Point c;
 } Triangle;
+
+struct Vec2
+{
+	int x;
+	int y;
+
+	Vec2() : x(0), y(0) {}		
+	Vec2(int x_, int y_) : x(x_), y(y_) {} // member initialization list
+
+	Vec2 operator+ (const Vec2 &v) const
+	{
+		Vec2 result;
+		result.x = x + v.x;
+		result.y = y + v.y;
+		return result;
+	}
+
+	Vec2 operator- (const Vec2 &v) const
+	{
+		Vec2 result;
+		result.x = x - v.x;
+		result.y = y - v.y;
+		return result;
+	}
+
+	Vec2 operator* (const double &c) const // scalar multiplication
+	{
+		Vec2 result;
+		result.x = x * c;
+		result.y = y * c;
+		return result;
+	}
+
+	Vec2 operator/ (const double &c) const // scalar division
+	{
+		Vec2 result;
+		result.x = x / c;
+		result.y = y / c;
+		return result;
+	}
+
+	double getMagnitude() const
+	{
+		return sqrt(x * x + y * y);
+	}
+
+	double dot(const Vec2 &v) const
+	{
+		return ((double)x * (double)v.x) + ((double)y * (double)v.y);
+	}
+
+	Vec2 getNormalized() const
+	{
+		Vec2 result;
+		double mag = this->getMagnitude();
+
+		if (mag > 0)
+		{
+			result.x = x / mag;
+			result.y = y / mag;
+		}
+		return result;
+	}
+
+	Vec2 getNormal() // returns the outward unit normal vector to a given vector following the left to right convention (counter-clockwise rotation)
+	{
+		return Vec2(-y, x).getNormalized();
+	}
+};
+
+Vec2 points2Vec(const Point &src, const Point &dst) // returns a vector from a source point towards a destination point
+{
+	return Vec2(dst.x - src.x, dst.y - src.y);
+}
+
+Point vec2Point(const Vec2 &v)
+{
+	return Point(static_cast<int>(round(v.x)), static_cast<int>(round(v.y)));
+}
 
 #endif
